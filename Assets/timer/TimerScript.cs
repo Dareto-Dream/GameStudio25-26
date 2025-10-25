3a5b6a60c9b7259c6a68033af458e41afb982e28
owl2708 <danieloya@student.midpac.edu>
2025-10-25 10:13:18 -1000

    added working clock to keep track of session time
    adds:
    - clock to screen
    - text for clook
    - a

---
+++ Assets/timer/TimerScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+public class TimerScript : MonoBehaviour
+{
+    public TMP_Text timerText; 
+    float elapsedTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("session time: " + "{0:00}:{1:00}", minutes, seconds);
+    }
+}
